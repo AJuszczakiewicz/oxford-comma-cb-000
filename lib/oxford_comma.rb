af3a98af301ab99a446220a6cd416c379b35deb1
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-10 12:16:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,13 +2,12 @@ def oxford_comma(array)
   array_size = array.size
   case array_size
   when 0,1
-    array.join()
+    phrase = array.join()
   when 2
-    array.join(", ")
+    phrase = array.join(", ")
   when array > 2
     last = array.pop
-    array.join!(', ')
-    array << " and #{last}"
+    phrase = array.join!(', ')
+    phrase << " and #{last}"
   end
-  array
 end
