c604d898599b52291e047ff5b2b61890737839e2
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-10 12:15:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  array_size = array.size
+  case array_size
+  when 0,1
+    array.join()
+  when 2
+    array.join(", ")
+  when array > 2
+    last = array.pop
+    array.join!(', ')
+    array << " and #{last}"
 end
